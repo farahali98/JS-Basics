a7cc191f4b6bb550a5320487a741c4d4e99b0764
Farah-Ali <farah.ali9805@gmail.com>
2020-10-01 16:02:30 +0300

    Step 4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+var name = prompt("please enter your first name")
+var surname = prompt("please enter last name");
+var city = prompt("where do you live?");
+
+
+alert("Hello  : " + name + "\n" + "Prenome : " + surname + "\n" + "Ville : " + city);
